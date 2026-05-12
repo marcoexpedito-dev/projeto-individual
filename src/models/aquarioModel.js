10eba68077f1959a9c3b964e5082360aaf27d927
marcoexpedito-dev <marco.gomes@sptech.school>
2026-05-12 14:58:59 -0300

    alteração 12-05

--- src/models/aquarioModel.js
+++ src/models/aquarioModel.js
@@ -1,16 +1,16 @@
 var database = require("../database/config");
 
-function buscarAquariosPorEmpresa(empresaId) {
+function buscarAquariosPorEmpresa(id) {
 
-  var instrucaoSql = `SELECT * FROM aquario a WHERE fk_empresa = ${empresaId}`;
+  var instrucaoSql = `SELECT * FROM usuario a WHERE id = ${id}`;
 
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
 }
 
-function cadastrar(empresaId, descricao) {
+function cadastrar(id, nome, email, senha) {
   
-  var instrucaoSql = `INSERT INTO (descricao, fk_empresa) aquario VALUES (${descricao}, ${empresaId})`;
+  var instrucaoSql = `INSERT INTO (id, nome, email, senha) usuario VALUES (${id}, ${nome}, ${email}, ${senha})`;
 
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
